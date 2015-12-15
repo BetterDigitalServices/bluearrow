3b2889cd106ca37c8b6c301069206b7d8aa1068c
Lauri Svan <lauri.svan@sc5.io>
2015-12-15 11:21:14 +0200

    Fix grid centering

--- wordpress/wp-content/themes/bluearrow/templates/footer.php
+++ wordpress/wp-content/themes/bluearrow/templates/footer.php
@@ -23,8 +23,11 @@
     </div>
   </div>
   <div class="container organiser-container">
+    <div class="row">
+      <h4 class ="col-md-offset-1">BLUE ARROW IS ORGANISED BY BETTER DIGITAL SERVICES FINLAND:</h4>
+    </div>
     <div class="row row-centered">
-      <div class="col-md-1 col-centered col-md-offset-1">
+      <div class="col-md-1 col-centered">
         <img class="img-responsive organiser" alt="Blue Arrow" src="wp-content/themes/bluearrow/dist/images/ba_company-17.svg">
       </div>
       <div class="col-md-1 col-centered">
@@ -53,6 +56,9 @@
       </div>
       <div class="col-md-1 col-centered">
         <img class="img-responsive companies" alt="Blue Arrow" src="wp-content/themes/bluearrow/dist/images/ba_company-26.svg">
+      </div>
+      <div class="col-md-1 col-centered">
+        <img class="img-responsive companies" alt="Blue Arrow" src="wp-content/themes/bluearrow/dist/images/ba_company-27.svg">
       </div>
     </div>
   </div>
