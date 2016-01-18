c1170e8941730c935aa936098bca66d775f8be89
Juha-Pekka Rajaniemi <juha.rajaniemi@gmail.com>
2016-01-18 22:39:46 +0200

    Tweak logo and category page style

--- wordpress/wp-content/themes/bluearrow/template-category.php
+++ wordpress/wp-content/themes/bluearrow/template-category.php
@@ -6,15 +6,6 @@
 ?>
 <?php while (have_posts()) : the_post(); ?>
     <div id="page-category">
-        <div class="row">
-            <div class="col-md-2 col-md-offset-1">
-                <div class="rotate">
-                    <h4><?= Titles\title(); ?></h4>
-                </div>
-            </div>
-            <div class="col-md-8">
-                <?php get_template_part('templates/content', 'page'); ?>
-            </div>
-        </div>
+        <?php get_template_part('templates/content', 'page'); ?>
     </div>
 <?php endwhile; ?>
