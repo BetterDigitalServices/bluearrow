16b5f63054f74eb8aca0de3e023060e8c9e7c5e4
Yrjö Kari-Koskinen <yrjo.kari-koskinen@futurice.com>
2016-01-24 00:46:35 +0200

    add favicon

--- wordpress/wp-content/themes/bluearrow/templates/head.php
+++ wordpress/wp-content/themes/bluearrow/templates/head.php
@@ -2,6 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="/wp-content/themes/bluearrow/dist/images/favicon.png" type="image/x-icon" />
   <base href="/">
   <?php wp_head(); ?>
 </head>
