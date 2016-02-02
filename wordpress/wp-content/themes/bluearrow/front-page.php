29e1d5578ba680d11ca82221d31e4574600db0a0
Juha-Pekka Rajaniemi <juha.rajaniemi@gmail.com>
2016-02-02 15:47:15 +0200

    Added twitter feed to frontpage

--- wordpress/wp-content/themes/bluearrow/front-page.php
+++ wordpress/wp-content/themes/bluearrow/front-page.php
@@ -18,6 +18,15 @@
 
 
     <?php get_template_part('templates/content', 'page'); ?>
+
+    <div class="row twitterstream">
+        <div class="col-sm-8 col-sm-offset-2">
+            <div class="col-md-9 col-md-offset-3">
+                <a class="twitter-timeline" href="https://twitter.com/bluearrowawards" data-widget-id="694509091348180993">Twiittejä käyttäjältä @bluearrowawards</a>
+                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php endwhile; ?>
