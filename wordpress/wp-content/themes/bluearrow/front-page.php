ce98691292996f52a1be0520b797a19d67bbe594
Juha-Pekka Rajaniemi <juha.rajaniemi@gmail.com>
2016-02-02 16:18:59 +0200

    New contact area to frontpage

--- wordpress/wp-content/themes/bluearrow/front-page.php
+++ wordpress/wp-content/themes/bluearrow/front-page.php
@@ -19,9 +19,25 @@
 
     <?php get_template_part('templates/content', 'page'); ?>
 
+    <div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
+            <div class="col-md-9 col-md-offset-3 socialmediaicons">
+                <a href="https://www.facebook.com/bluearrowawards/">
+                    <img src="wp-content/themes/bluearrow/dist/images/icon-fb.svg" alt="Blue Arrow at Facebook">
+                </a>
+                <a href="https://twitter.com/bluearrowawards">
+                    <img src="wp-content/themes/bluearrow/dist/images/icon-twitter.svg" alt="Blue Arrow at Twitter">
+                </a>
+                <a href="https://www.linkedin.com/groups/8465932">
+                    <img src="wp-content/themes/bluearrow/dist/images/icon-linkedin.svg" alt="Blue Arrow at LinkedIn">
+                </a>
+            </div>
+        </div>
+    </div>
+
     <div class="row twitterstream">
         <div class="col-sm-8 col-sm-offset-2">
-            <div class="col-md-9 col-md-offset-3">
+            <div class="col-md-9 col-md-offset-3" style="padding-left: 0">
                 <a class="twitter-timeline" href="https://twitter.com/bluearrowawards" data-widget-id="694509091348180993">Twiittejä käyttäjältä @bluearrowawards</a>
                 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
             </div>
