e42b627a862ba79a29c103c77834bcb92d3fc08c
Lauri Svan <lauri.svan@sc5.io>
2016-03-16 09:28:04 +0200

    Add Nitor logo; still some responsive trouble

--- wordpress/wp-content/themes/bluearrow/templates/footer.php
+++ wordpress/wp-content/themes/bluearrow/templates/footer.php
@@ -36,7 +36,7 @@
     </div>
 -->
     <div class="row row-centered">
-      <div class="col-md-1 col-centered">
+      <div class="col-sm-1 col-centered">
         <a href="http://codento.fi/">
           <img class="img-responsive organiser" alt="Codento" src="wp-content/themes/bluearrow/dist/images/codento.svg">
         </a>
@@ -58,7 +58,7 @@
       </div>
       <div class="col-md-1 col-centered">
         <a href="http://futurice.com/">
-          <img class="img-responsive companies" alt="Futurice" src="wp-content/themes/bluearrow/dist/images/futurice.svg">
+          <img class="img-responsive organiser" alt="Futurice" src="wp-content/themes/bluearrow/dist/images/futurice.svg">
         </a>
       </div>
       <div class="col-md-1 col-centered">
@@ -66,12 +66,19 @@
           <img class="img-responsive organiser" alt="Gofore" src="wp-content/themes/bluearrow/dist/images/gofore.svg">
         </a>
       </div>
+    </div>
+    <div class="row row-centered">
       <div class="col-md-1 col-centered">
         <a href="http://www.houston-inc.com/">
           <img class="img-responsive organiser" alt="Houston Inc." src="wp-content/themes/bluearrow/dist/images/houston-inc.svg">
         </a>
       </div>
-        <div class="col-md-1 col-centered">
+      <div class="col-md-1 col-centered">
+        <a href="http://www.nitor.fi/">
+          <img class="img-responsive organiser" alt="Nitor" src="wp-content/themes/bluearrow/dist/images/nitor.svg">
+        </a>
+      </div>
+      <div class="col-md-1 col-centered">
         <a href="http://reaktor.com/">
           <img class="img-responsive organiser" alt="Reaktor" src="wp-content/themes/bluearrow/dist/images/reaktor.svg">
         </a>
@@ -88,7 +95,7 @@
       </div>
       <div class="col-md-1 col-centered">
         <a href="https://www.vincit.fi/">
-          <img class="img-responsive companies" alt="Vincit" src="wp-content/themes/bluearrow/dist/images/vincit.svg">
+          <img class="img-responsive organiser" alt="Vincit" src="wp-content/themes/bluearrow/dist/images/vincit.svg">
         </a>
       </div>
     </div>
