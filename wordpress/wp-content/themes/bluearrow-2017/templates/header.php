afe572922ec9966f45f13d5fecbc2a1651cabd2d
Johannes Siipola <johannes@exove.fi>
2016-12-29 10:29:39 +0200

    Initial version of the new header

--- wordpress/wp-content/themes/bluearrow-2017/templates/header.php
+++ wordpress/wp-content/themes/bluearrow-2017/templates/header.php
@@ -1,12 +1,32 @@
-<header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
+<?php
+
+$menuItems = [
+  [
+    'title' => 'News',
+    'url' => 'news',
+    'icon' => 'header-icon-news.svg'
+  ],
+  [
+    'title' => 'Categories',
+    'url' => 'categories',
+    'icon' => 'header-icon-categories.svg'
+  ]
+]
+
+?>
+
+<header class="header">
+    <nav class="main-nav">
+      <a class="header-logo" href="<?php echo esc_url(home_url('/')); ?>"></a>
+      <ul class="main-nav-links">
+        <?php foreach ($menuItems as $item): ?>
+        <li class="main-nav-links-item">
+          <a class="main-nav-links-item-link" href="<?php echo esc_url(home_url($item['url'])); ?>">
+            <div class="main-nav-links-item-icon" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/' . $item['icon'] ?>')"></div>
+            <div class="main-nav-links-item-title"><?php echo $item['title'] ?></div>
+          </a>
+        </li>
+        <?php endforeach; ?>
+      </ul>
     </nav>
-  </div>
 </header>
