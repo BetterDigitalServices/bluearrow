20b0770d7dc614d23c5ba973c1c0113fc36a6bce
Johannes Siipola <johannes@exove.fi>
2016-12-29 15:43:54 +0200

    Add front page template

---
+++ wordpress/wp-content/themes/bluearrow-2017/front-page.php
@@ -0,0 +1,36 @@
+<h1>Blue Arrow. An award for impact.</h1>
+<div class="clearfix">
+  <div class="squiggle util-background-image"></div>
+</div>
+<div class="front-page-top-section">
+  <div class="row">
+    <div class="col-md-3">
+    </div>
+    <div class="col-md-9"
+      <p>Blue Arrow awards selects the best services, teams, people and code in digital industry in 8 categories.</p>
+      <p>We are looking for quality and depth in concept, design and code.</p>
+    </div>
+  </div>
+</div>
+<div class="front-page-section">
+  <div class="row">
+    <div class="col-md-3">
+      <div class="front-page-section-icon util-background-icon" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-large-clock.svg' ?>')"></div>
+    </div>
+    <div class="col-md-9">
+      <h2>Schedule</h2>
+      <p>The competition is open for entries from 25.1.2017 to 29.2.2017. Enter your product, service or campaign if you feel it has made real impact.</p>
+    </div>
+  </div>
+</div>
+<div class="front-page-section">
+  <div class="row">
+    <div class="col-md-3">
+      <div class="front-page-section-icon util-background-icon" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-large-champagne.svg' ?>')"></div>
+    </div>
+    <div class="col-md-9">
+      <h2>Gala</h2>
+      <p>The award gala will take place in April 2017</p>
+    </div>
+  </div>
+</div>
