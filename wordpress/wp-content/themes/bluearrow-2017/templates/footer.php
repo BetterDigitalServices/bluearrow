b7a9e6694270b6604daafeee85a89652a23219c3
Johannes Siipola <johannes@exove.fi>
2016-12-31 00:27:56 +0200

    Add footer logos

--- wordpress/wp-content/themes/bluearrow-2017/templates/footer.php
+++ wordpress/wp-content/themes/bluearrow-2017/templates/footer.php
@@ -1,5 +1,13 @@
-<footer class="content-info">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+<footer class="content-info footer">
+  <div class="container-fluid">
+    <div class="logo-container clearfix">
+    <a class="footer-logo util-background-icon" href="http://www.exove.com" target="_blank" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/logo-exove.svg' ?>')"></a>
+    <a class="footer-logo util-background-icon" href="https://futurice.com/" target="_blank" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/logo-futurice.svg' ?>')"></a>
+    <a class="footer-logo util-background-icon" href="https://gofore.com/" target="_blank" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/logo-gofore.svg' ?>')"></a>
+    <a class="footer-logo util-background-icon" href="http://www.houston-inc.com/" target="_blank" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/logo-houston.svg' ?>')"></a>
+    <a class="footer-logo util-background-icon" href="https://www.reaktor.com/" target="_blank" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/logo-reaktor.svg' ?>')"></a>
+    <a class="footer-logo util-background-icon" href="https://www.solita.fi/" target="_blank" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/logo-solita.svg' ?>')"></a>
+    <a class="footer-logo util-background-icon" href="https://www.vincit.fi/" target="_blank" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/logo-vincit.svg' ?>')"></a>
+    </div>
   </div>
 </footer>
