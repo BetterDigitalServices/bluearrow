7bbe2a66e0a6bc088d4f8d2a42d223ed961cc41f
Johannes Siipola <johannes@exove.fi>
2017-01-02 11:51:29 +0200

    Add favicons

--- wordpress/wp-content/themes/bluearrow-2017/templates/head.php
+++ wordpress/wp-content/themes/bluearrow-2017/templates/head.php
@@ -2,5 +2,12 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="apple-touch-icon" sizes="180x180" href="/wp-content/themes/bluearrow-2017/assets/images/favicons/apple-touch-icon.png?v=aljoyN4WAB">
+  <link rel="icon" type="image/png" href="/wp-content/themes/bluearrow-2017/assets/images/favicons/favicon-32x32.png?v=aljoyN4WAB" sizes="32x32">
+  <link rel="icon" type="image/png" href="/wp-content/themes/bluearrow-2017/assets/images/favicons/favicon-16x16.png?v=aljoyN4WAB" sizes="16x16">
+  <link rel="manifest" href="/wp-content/themes/bluearrow-2017/assets/images/favicons/manifest.json?v=aljoyN4WAB">
+  <link rel="mask-icon" href="/wp-content/themes/bluearrow-2017/assets/images/favicons/safari-pinned-tab.svg?v=aljoyN4WAB" color="#5cadc2">
+  <link rel="shortcut icon" href="/wp-content/themes/bluearrow-2017/assets/images/favicons/favicon.ico?v=aljoyN4WAB">
+  <meta name="msapplication-config" content="/wp-content/themes/bluearrow-2017/assets/images/favicons/browserconfig.xml?v=aljoyN4WAB">
   <?php wp_head(); ?>
 </head>
