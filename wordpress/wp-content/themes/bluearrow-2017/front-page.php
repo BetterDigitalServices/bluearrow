d14a0b5b5ca77d51778219022403319c3a768a94
Johannes Siipola <johannes@exove.fi>
2017-01-03 16:43:20 +0200

    Update front page layout

--- wordpress/wp-content/themes/bluearrow-2017/front-page.php
+++ wordpress/wp-content/themes/bluearrow-2017/front-page.php
@@ -1,13 +1,7 @@
 <h1 class="front-page-title">Blue Arrow. An award for impact.</h1>
-<div class="clearfix">
-  <div class="squiggle util-background-image"></div>
-</div>
 <div class="front-page-top-section">
   <div class="row">
-    <div class="col-md-3">
-      <div class="arrow util-background-icon"></div>
-    </div>
-    <div class="col-md-9"
+    <div class="col-xs-12"
       <p>Blue Arrow awards selects the best services, teams, people and code in digital industry in 8 categories.</p>
       <p>We are looking for quality and depth in concept, design and code.</p>
     </div>
@@ -26,8 +20,16 @@
   </div>
 </div>
 <div class="row">
-  <div class="front-page-section col-lg-6 offset-lg-3">
+  <div class="front-page-section col-sm-12">
     <div class="front-page-section-icon util-background-icon" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-large-categories.svg' ?>')"></div>
     <h2>Categories</h2>
+    <div class="categories-container clearfix">
+      <?php foreach(range(1,8) as $item): ?>
+        <div class="category-box">
+          <div class="category-box-corner"></div>
+          <div class="category-box-title">Societal Influence</div>
+        </div>
+      <?php endforeach; ?>
+    </div>
   </div>
 </div>
