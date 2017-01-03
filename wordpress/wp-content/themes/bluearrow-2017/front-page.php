404a9e9d6897356cdf62c9f6414edd736fc4acf0
Johannes Siipola <johannes@exove.fi>
2017-01-03 14:52:21 +0200

    Update front page template

--- wordpress/wp-content/themes/bluearrow-2017/front-page.php
+++ wordpress/wp-content/themes/bluearrow-2017/front-page.php
@@ -13,25 +13,21 @@
     </div>
   </div>
 </div>
-<div class="front-page-section">
-  <div class="row">
-    <div class="col-md-3">
-      <div class="front-page-section-icon util-background-icon" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-large-clock.svg' ?>')"></div>
-    </div>
-    <div class="col-md-9">
-      <h2>Schedule</h2>
-      <p>The competition is open for entries from <span class="text-highlight">25.1.2017</span> to <span class="text-highlight">29.2.2017.</span> Enter your product, service or campaign if you feel it has made real impact.</p>
-    </div>
+<div class="row">
+  <div class="front-page-section col-lg-6">
+    <div class="front-page-section-icon util-background-icon" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-large-clock.svg' ?>')"></div>
+    <h2>Schedule</h2>
+    <p>The competition is open for entries from <span class="text-highlight">25.1.2017</span> to <span class="text-highlight">29.2.2017.</span> Enter your product, service or campaign if you feel it has made real impact.</p>
   </div>
-</div>
-<div class="front-page-section">
-  <div class="row">
-    <div class="col-md-3">
+  <div class="front-page-section col-lg-6">
       <div class="front-page-section-icon util-background-icon" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-large-champagne.svg' ?>')"></div>
-    </div>
-    <div class="col-md-9">
       <h2>Gala</h2>
       <p>The award gala will take place in <span class="text-highlight">April 2017</span></p>
-    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="front-page-section col-lg-6 offset-lg-3">
+    <div class="front-page-section-icon util-background-icon" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-large-categories.svg' ?>')"></div>
+    <h2>Categories</h2>
   </div>
 </div>
