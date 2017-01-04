d774322c2023c7e491b4a4a2bc7cb306c3c4e30d
Johannes Siipola <johannes@exove.fi>
2017-01-04 15:23:17 +0200

    Add header button

--- wordpress/wp-content/themes/bluearrow-2017/templates/header.php
+++ wordpress/wp-content/themes/bluearrow-2017/templates/header.php
@@ -7,40 +7,46 @@ if (isset($menu_locations['primary_navigation'])) {
 ?>
 
 <header class="header">
-    <nav class="main-nav">
+    <nav class="main-nav clearfix">
       <a class="header-logo" href="<?php echo esc_url(home_url('/')); ?>"></a>
-      <ul class="main-nav-links">
-        <?php foreach ($menu as $item): ?>
-        <li class="main-nav-links-item">
-          <a class="main-nav-links-item-link" href="<?php echo esc_url($item->url); ?>">
-            <?php $icon = get_post_meta( $item->ID, 'menu-item-icon', true ); ?>
-            <?php if($icon): ?>
-              <div class="main-nav-links-item-icon" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/' . $icon ?>')"></div>
-            <?php else: ?>
-              <div class="main-nav-links-item-icon" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/header-icon-news.svg' ?>')"></div>
-            <?php endif; ?>
-            <div class="main-nav-links-item-title"><?php echo $item->title ?></div>
-          </a>
-        </li>
-        <?php endforeach; ?>
-      </ul>
+      <div class="header-right">
+        <ul class="main-nav-links">
+          <?php foreach ($menu as $item): ?>
+          <li class="main-nav-links-item">
+            <a class="main-nav-links-item-link" href="<?php echo esc_url($item->url); ?>">
+              <?php $icon = get_post_meta( $item->ID, 'menu-item-icon', true ); ?>
+              <?php if($icon): ?>
+                <div class="main-nav-links-item-icon" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/' . $icon ?>')"></div>
+              <?php else: ?>
+                <div class="main-nav-links-item-icon" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/header-icon-news.svg' ?>')"></div>
+              <?php endif; ?>
+              <div class="main-nav-links-item-title"><?php echo $item->title ?></div>
+            </a>
+          </li>
+          <?php endforeach; ?>
+        </ul>
+        <div class="header-button">
+          <div class="header-button-text">Entries open 31.1.2017</div>
+        </div>
+      </div>
       <div class="header-mobile-nav-button util-background-icon"></div>
     </nav>
 </header>
 <div class="mobile-nav-menu">
-    <ul class="mobile-nav-menu-list">
-        <?php foreach ($menu as $item): ?>
-            <li class="mobile-nav-menu-item">
-                <a class="mobile-nav-menu-item-link" href="<?php echo esc_url($item->url); ?>">
-                    <?php $icon = get_post_meta( $item->ID, 'menu-item-icon', true ); ?>
-                    <?php if($icon): ?>
-                        <div class="mobile-nav-menu-item-icon util-background-icon" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/' . $icon ?>')"></div>
-                    <?php else: ?>
-                        <div class="mobile-nav-menu-item-icon util-background-icon" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/header-icon-news.svg' ?>')"></div>
-                    <?php endif; ?>
-                    <div class="mobile-nav-menu-item-title"><?php echo $item->title ?></div>
-                </a>
-            </li>
-        <?php endforeach; ?>
-    </ul>
+  <ul class="mobile-nav-menu-list">
+    <?php foreach ($menu as $item): ?>
+      <li class="mobile-nav-menu-item">
+        <a class="mobile-nav-menu-item-link" href="<?php echo esc_url($item->url); ?>">
+          <?php $icon = get_post_meta( $item->ID, 'menu-item-icon', true ); ?>
+          <?php if($icon): ?>
+              <div class="mobile-nav-menu-item-icon util-background-icon" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/' . $icon ?>')"></div>
+          <?php else: ?>
+              <div class="mobile-nav-menu-item-icon util-background-icon" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/header-icon-news.svg' ?>')"></div>
+          <?php endif; ?>
+          <div class="mobile-nav-menu-item-title"><?php echo $item->title ?></div>
+        </a>
+      </li>
+    <?php endforeach; ?>
+  </ul>
 </div>
+
