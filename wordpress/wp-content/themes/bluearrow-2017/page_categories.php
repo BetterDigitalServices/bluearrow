7aa4d29bffc5773ad51131f63d87757378051caa
Johannes Siipola <johannes@exove.fi>
2017-01-19 14:19:50 +0200

    Fix category scroll anchor position

--- wordpress/wp-content/themes/bluearrow-2017/page_categories.php
+++ wordpress/wp-content/themes/bluearrow-2017/page_categories.php
@@ -16,49 +16,52 @@
     </div>
     <?php echo wpautop(get_post_field('post_content', $post->ID)); ?>
     <?php foreach ($categories as $category): ?>
-      <div class="category-spacer"></div>
-      <p class="category-subtitle"><?php echo get_field('subtitle', $category->ID) ?></p>
-      <h2 class="category-title" id="<?php echo $category->post_name; ?>">
-        <?php echo $category->post_title; ?>
-      </h2>
-      <?php echo wpautop(get_post_field('post_content', $category->ID)); ?>
-      <?php $judge = get_field('main_judge', $category->ID)[0] ?>
-      <?php if ($judge): ?>
-      <div class="category-main-judge">
-        <div class="row category-main-judge-header">
-          <div class="col-md-3">
-            <img src="<?php echo get_the_post_thumbnail_url($judge) ?>" class="img-circle img-fluid category-main-judge-image" alt="<?php echo $judge->post_title ?>">
+      <article class="category">
+        <div class="category-anchor" id="<?php echo $category->post_name; ?>"></div>
+        <div class="category-spacer"></div>
+        <p class="category-subtitle"><?php echo get_field('subtitle', $category->ID) ?></p>
+        <h2 class="category-title">
+          <?php echo $category->post_title; ?>
+        </h2>
+        <?php echo wpautop(get_post_field('post_content', $category->ID)); ?>
+        <?php $judge = get_field('main_judge', $category->ID)[0] ?>
+        <?php if ($judge): ?>
+        <div class="category-main-judge">
+          <div class="row category-main-judge-header">
+            <div class="col-md-3">
+              <img src="<?php echo get_the_post_thumbnail_url($judge) ?>" class="img-circle img-fluid category-main-judge-image" alt="<?php echo $judge->post_title ?>">
+            </div>
+            <div class="col-md-9 category-main-judge-details-wrapper">
+              <div class="category-main-judge-details">
+                <div class="category-main-judge-title">Main Judge</div>
+                <div class="category-main-judge-name"><?php echo $judge->post_title ?></div>
+                <div class="category-main-judge-short-description"><?php echo get_post_field('short_description', $judge->ID); ?></div>
+              </div>
+            </div>
           </div>
-          <div class="col-md-9 category-main-judge-details-wrapper">
-            <div class="category-main-judge-details">
-              <div class="category-main-judge-title">Main Judge</div>
-              <div class="category-main-judge-name"><?php echo $judge->post_title ?></div>
-              <div class="category-main-judge-short-description"><?php echo get_post_field('short_description', $judge->ID); ?></div>
+          <div class="row">
+            <div class="col-xs-12">
+              <div class="category-main-judge-long-description"><?php echo get_post_field('long_description', $judge->ID); ?></div>
             </div>
           </div>
         </div>
-        <div class="row">
-          <div class="col-xs-12">
-            <div class="category-main-judge-long-description"><?php echo get_post_field('long_description', $judge->ID); ?></div>
+        <?php endif; ?>
+        <?php $assisting_jury = get_field('judges', $category->ID) ?>
+        <?php if ($assisting_jury): ?>
+          <h3 class="category-assisting-jury-title">Assisting jury</h3>
+          <div class="row flex-items-xs-center">
+            <?php foreach ($assisting_jury as $judge): ?>
+              <div class="col-md-4 category-assisting-judge">
+                <img src="<?php echo get_the_post_thumbnail_url($judge) ?>" class="img-circle img-fluid category-assisting-judge-image" alt="<?php echo $judge->post_title ?>">
+                <div class="category-assisting-judge-name"><?php echo $judge->post_title ?></div>
+                <div class="category-assisting-judge-short-description"><?php echo get_post_field('short_description', $judge->ID) ?></div>
+              </div>
+            <?php endforeach; ?>
           </div>
-        </div>
-      </div>
-      <?php endif; ?>
-      <?php $assisting_jury = get_field('judges', $category->ID) ?>
-      <?php if ($assisting_jury): ?>
-        <h3 class="category-assisting-jury-title">Assisting jury</h3>
-        <div class="row flex-items-xs-center">
-          <?php foreach ($assisting_jury as $judge): ?>
-            <div class="col-md-4 category-assisting-judge">
-              <img src="<?php echo get_the_post_thumbnail_url($judge) ?>" class="img-circle img-fluid category-assisting-judge-image" alt="<?php echo $judge->post_title ?>">
-              <div class="category-assisting-judge-name"><?php echo $judge->post_title ?></div>
-              <div class="category-assisting-judge-short-description"><?php echo get_post_field('short_description', $judge->ID) ?></div>
-            </div>
-          <?php endforeach; ?>
-        </div>
-      <?php endif; ?>
-      <h3 class="category-evaluation-criteria-title">Evaluation criteria</h3>
-      <?php echo get_field('evaluation_criteria', $category->ID) ?>
+        <?php endif; ?>
+        <h3 class="category-evaluation-criteria-title">Evaluation criteria</h3>
+        <?php echo get_field('evaluation_criteria', $category->ID) ?>
+      </article>
     <?php endforeach; ?>
   </div>
 </div>
