3bc2530c9cfa8b9dc6011de85edda0104af5e43c
Johannes Siipola <johannes@exove.fi>
2017-01-19 14:10:42 +0200

    Make main judge block two-column

--- wordpress/wp-content/themes/bluearrow-2017/page_categories.php
+++ wordpress/wp-content/themes/bluearrow-2017/page_categories.php
@@ -25,12 +25,23 @@
       <?php $judge = get_field('main_judge', $category->ID)[0] ?>
       <?php if ($judge): ?>
       <div class="category-main-judge">
-        <div class="category-main-judge-icon util-background-icon" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/icon-large-judge.svg' ?>')"></div>
-        <div class="category-main-judge-title">Main Judge</div>
-        <div class="category-main-judge-name"><?php echo $judge->post_title ?></div>
-        <img src="<?php echo get_the_post_thumbnail_url($judge) ?>" class="img-circle img-fluid category-main-judge-image" alt="<?php echo $judge->post_title ?>">
-        <div class="category-main-judge-short-description"><?php echo get_post_field('short_description', $judge->ID); ?></div>
-        <div class="category-main-judge-long-description"><?php echo get_post_field('long_description', $judge->ID); ?></div>
+        <div class="row category-main-judge-header">
+          <div class="col-md-3">
+            <img src="<?php echo get_the_post_thumbnail_url($judge) ?>" class="img-circle img-fluid category-main-judge-image" alt="<?php echo $judge->post_title ?>">
+          </div>
+          <div class="col-md-9 category-main-judge-details-wrapper">
+            <div class="category-main-judge-details">
+              <div class="category-main-judge-title">Main Judge</div>
+              <div class="category-main-judge-name"><?php echo $judge->post_title ?></div>
+              <div class="category-main-judge-short-description"><?php echo get_post_field('short_description', $judge->ID); ?></div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-12">
+            <div class="category-main-judge-long-description"><?php echo get_post_field('long_description', $judge->ID); ?></div>
+          </div>
+        </div>
       </div>
       <?php endif; ?>
       <?php $assisting_jury = get_field('judges', $category->ID) ?>
